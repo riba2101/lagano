c418f05a3031ec53ce3ac2e2ff1cb431a126257e
ORibicic <oribicic@gmail.com>
2023-05-23 19:17:30 +0200

    wip

--- lagano-plugin/lagano-jooq-maven-plugin/src/test/kotlin/org/laganini/lagano/codegen/maven/LaganoPluginIT.kt
+++ lagano-plugin/lagano-jooq-maven-plugin/src/test/kotlin/org/laganini/lagano/codegen/maven/LaganoPluginIT.kt
@@ -1,24 +1,29 @@
 package org.laganini.lagano.codegen.maven
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase
+import org.apache.maven.plugin.testing.MojoRule
+import org.apache.maven.project.MavenProject
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
 
 
-open class LaganoPluginIT : AbstractMojoTestCase() {
+class LaganoPluginIT : AbstractMojoTestCase() {
 
-    @BeforeEach
-    override fun setUp() {
-        super.setUp()
-    }
-
-    @Test
-    fun shouldRunEmpty() {
-        val pluginXml = File(getBasedir(), "src/test/resources/units/empty/pom.xml")
-        val mojo: LaganoPlugin = lookupMojo("lagano-generate", pluginXml) as LaganoPlugin
-
-        mojo.execute()
-    }
+//    private val mojoRule: MojoRule = MojoRule(this)
+//
+//    @BeforeEach
+//    override fun setUp() {
+//        super.setUp()
+//    }
+//
+//    @Test
+//    fun shouldRunEmpty() {
+//        val pluginXml = File(getBasedir(), "src/test/resources/units/empty")
+//        val project: MavenProject? = mojoRule.readMavenProject(pluginXml)
+//        val mojo: LaganoPlugin = lookupConfiguredMojo(project, "lagano-generate") as LaganoPlugin
+//
+//        mojo.execute()
+//    }
 
 }
